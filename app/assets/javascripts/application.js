758109ac508aa13c9175b5614d1d2ddc1c62d1b0
vananrajaindern <vananrajaindern@gmail.com>
2017-05-25 16:09:20 +0800

    [thredded_create_app] Add jQuery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery3
 //= require rails-ujs
+//= require jquery3
 //= require turbolinks
+//= require jquery3
 //= require_tree .
